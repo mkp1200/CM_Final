54f3eb83c7b4bb61713d256647141be3775abc97
mkp1200 <mkpryor1200@gmail.com>
2022-07-24 10:57:58 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import clubmedia from './images/clubmedia.png';
+import useStyles from './styles';
+
+
+const App = () => {
+  const [currentID, setCurrentID] = useState(null);
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      dispatch(getPosts());
+    }, [dispatch]);
+    return (
+        <Container maxWidth="lg">
+            <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Club Media</Typography>
+        <img className={classes.image} src={clubmedia} alt="icon" height="60" />
+      </AppBar>
+            <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+                <Posts setCurrentID = {setCurrentID} />
+              
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Form currentID = {currentID} setCurrentID={setCurrentID} />
+              
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+export default App;
